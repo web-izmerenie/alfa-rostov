0c68c36c361dede36fd1215fc6cc5621956ed33c
www-data <www-data@Debian-60-squeeze-64-minimal.(none)>
2014-07-21 19:11:06 +0400

    new deploy system

---
+++ _deploy/13-grunt_default_tasks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ -z "$PARENT_DEPLOY_SCRIPT" ]; then YOUR_SUBJECT="./_deploy/$(basename "$0")" WD="$(dirname "$0")/../" ../deploy.sh; exit "$?"; fi
+
+info "Starting grunt default tasks"
+if ./grunt; then
+	info_inline "Grunt default tasks status:"; ok;
+else err; fi
--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
 # Author: Viacheslav Lotsmanov
-# License: GNU/GPLv3 by Free Software Foundation
+# License: GNU/GPLv3 by Free Software Foundation (https://github.com/unclechu/grunt-project-templates/blob/master/LICENSE)
 #
 
-PARENT_DEPLOY_SCRIPT=true
+PARENT_DEPLOY_SCRIPT=1
+
+[ -n "$WD" ] && cd "$WD" && unset WD
 
 clr_info='\e[0;36m'
 clr_ok='\e[0;32m'
@@ -20,158 +22,147 @@ clr_end='\e[0m'
 #   $4 - suffix (no color)
 #   $@ - info text
 function _info_pattern {
-    color="$clr_info"
+	color="$clr_info"
 
-    flags=$1
-    shift
-    write_to=$1
-    shift
-    color=$1
-    shift
-    suffix=$1
-    shift
+	flags=$1
+	shift
+	write_to=$1
+	shift
+	color=$1
+	shift
+	suffix=$1
+	shift
 
-    echo -e $flags "${color}${@}${clr_end}${suffix}" 1>&$write_to
+	echo -e $flags "${color}${@}${clr_end}${suffix}" 1>&$write_to
 }
 
 # simple info
 
 function info {
-    _info_pattern "" 1 "$clr_info" " ... " "$@"
+	_info_pattern "" 1 "$clr_info" " ... " "$@"
 }
 
 function info_ok {
-    _info_pattern "" 1 "$clr_ok" " ... " "$@"
+	_info_pattern "" 1 "$clr_ok" " ... " "$@"
 }
 
 function info_err {
-    _info_pattern "" 2 "$clr_err" " ... " "$@"
+	_info_pattern "" 2 "$clr_err" " ... " "$@"
 }
 
 # inline info
 
 function info_inline {
-    _info_pattern "-n" 1 "$clr_info" " ... " "$@"
+	_info_pattern "-n" 1 "$clr_info" " ... " "$@"
 }
 
 function info_ok_inline {
-    _info_pattern "-n" 1 "$clr_ok" " ... " "$@"
+	_info_pattern "-n" 1 "$clr_ok" " ... " "$@"
 }
 
 function info_err_inline {
-    _info_pattern "-n" 2 "$clr_err" " ... " "$@"
+	_info_pattern "-n" 2 "$clr_err" " ... " "$@"
 }
 
 # info clean
 
 function info_clean {
-    _info_pattern "" 1 "$clr_info" "" "$@"
+	_info_pattern "" 1 "$clr_info" "" "$@"
 }
 
 function info_ok_clean {
-    _info_pattern "" 1 "$clr_ok" "" "$@"
+	_info_pattern "" 1 "$clr_ok" "" "$@"
 }
 
 function info_err_clean {
-    _info_pattern "" 2 "$clr_err" "" "$@"
+	_info_pattern "" 2 "$clr_err" "" "$@"
 }
 
 # info inline clean
 
 function info_inline_clean {
-    _info_pattern "-n" 1 "$clr_info" "" "$@"
+	_info_pattern "-n" 1 "$clr_info" "" "$@"
 }
 
 function info_inline_ok_clean {
-    _info_pattern "-n" 1 "$clr_ok" "" "$@"
+	_info_pattern "-n" 1 "$clr_ok" "" "$@"
 }
 
 function info_inline_err_clean {
-    _info_pattern "-n" 2 "$clr_err" "" "$@"
+	_info_pattern "-n" 2 "$clr_err" "" "$@"
 }
 
 # run wrappers
 
 function run_inline_answer_silent {
-    errlog=$("${@}" 2>&1)
-    if [ $? -ne 0 ]; then
-        err 1
-        info_clean "Command: '${@}'"
-        info_err_clean "Error log: '${errlog}'"
-        exit 1
-    fi
+	errlog=$("${@}" 2>&1)
+	if [ $? -ne 0 ]; then
+		err 1
+		info_clean "Command: '${@}'"
+		info_err_clean "Error log: '${errlog}'"
+		exit 1
+	fi
 }
 
 function run_inline_answer {
-    run_inline_answer_silent "${@}"
-    ok
+	run_inline_answer_silent "${@}"
+	ok
 }
 
 # statuses
 
 function ok {
-    echo -e "${clr_ok}[ OK ]${clr_end}"
-    return 0
+	echo -e "${clr_ok}[ OK ]${clr_end}"
+	return 0
 }
 
 function err {
-    echo -e "${clr_err}[ ERR ]${clr_end}" 1>&2
-    [ -z $1 ] && exit 1
+	echo -e "${clr_err}[ ERR ]${clr_end}" 1>&2
+	[ -z $1 ] && exit 1
 }
 
 function ask {
-    echo -en "${clr_ask}${@}${clr_end} [Y/n] "
-    read answer
+	echo -en "${clr_ask}${@}${clr_end} [Y/n] "
+	read answer
 
-    if echo "$answer" | grep -i '^y\(es\)\?$' &>/dev/null; then
-        return 0
-    elif echo "$answer" | grep -i '^n\(o\)\?$' &>/dev/null; then
-        return 1
-    else
-        info_err_clean "Incorrect answer!"
-        exit 1
-    fi
+	if echo "$answer" | grep -i '^y\(es\)\?$' &>/dev/null; then
+		return 0
+	elif echo "$answer" | grep -i '^n\(o\)\?$' &>/dev/null; then
+		return 1
+	else
+		info_err_clean "Incorrect answer!"
+		exit 1
+	fi
 }
 
 function please_type {
-    echo -en "${clr_ask}${@}${clr_end}: "
+	echo -en "${clr_ask}${@}${clr_end}: "
 }
 
-# default deploy actions
+# deploy tasks
 
-info_inline "Checking for grunt"
-if [ -e ./node_modules/.bin/grunt ]; then ok; else
-    err 1;
-    info_err_clean "No grunt!" \
-        "You need to do \`${clr_info}npm install${clr_err}\`" \
-        "before deploy script!"
+if [ -n "$YOUR_SUBJECT" ]; then
+	source "$YOUR_SUBJECT"
+	unset YOUR_SUBJECT
+else
+	tasks=$(find ./_deploy -maxdepth 1 -type f | grep '\.sh$' | sort | tr '\n' ':')
+	
+	while [ -n "$tasks" ]; do
+		task="${tasks#*:}"
+		task="${tasks:0:$[ ${#tasks} - ${#task} - 1 ]}"
 
-    if ask "Do \`${clr_info}npm install${clr_ask}\` now?"; then
-        if ! npm install; then exit 1; fi
-        exit 0
-    else
-        info_clean "You need to do \`${clr_ask}npm install${clr_info}\` first."
-        exit 1
-    fi
+		info "Started task \"$task\""
+		source "$task"
+		info_inline "Task \"$task\" successfully finished" && ok
+
+		tasks="${tasks#*:}"
+	done
 fi
-
-info_inline "Creating symbolic link to grunt-cli"
-rm ./grunt &>/dev/null
-if ln -s ./node_modules/.bin/grunt ./grunt &>/dev/null; then ok; else err; fi
-
-info "Starting grunt default tasks"
-if ./grunt; then
-    info_inline "Grunt default tasks status:"; ok;
-else err; fi
-
-# custom deploy actions
-
-info_inline "Deprivation of privileges group and others"
-run_inline_answer chmod -R go-rwx .
 
 # finishing of deploying
 
-info_ok_clean "This repository is successfully deployed!"
-exit 0
+if [ -z "$YOUR_SUBJECT" ]; then
+	info_ok_clean "This repository is successfully deployed!"
+fi
 
-# vim:set ts=4 sw=4 et:
+exit 0
