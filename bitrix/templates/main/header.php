b99fe4091efc502d36d916c181b0b9a28e415b46
www-data <www-data@Debian-60-squeeze-64-minimal.(none)>
2014-08-13 11:41:15 +0400

    bug

--- bitrix/templates/main/header.php
+++ bitrix/templates/main/header.php
@@ -34,6 +34,7 @@
         
     $html_classes = implode(" ", $html_classes);
     
+    
 ?><!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?=LANGUAGE_ID?>" lang="<?=LANGUAGE_ID?>" class="<?=$html_classes?>">
 <head>
