534c3380a31c276f7c5636b3eae24722979a5a18
www-data <www-data@Debian-60-squeeze-64-minimal.(none)>
2014-08-13 12:20:26 +0400

    404 special bug

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Страница не найдена");
 ?>
 <div class="section_standart section_standart_wide">
-    <main>
+    <main style="text-align:center">
         <img src="/bitrix/templates/main/images/404.png" alt="Страница не найдена" />
         <h1>Ошибка 404</h1>
         <p>Введен неверный адрес, или такой страницы больше нет. <br /> Вернитесь на <a href="/">главную</a></p>
--- bitrix/templates/.default/components/bitrix/news.list/catalog.list/template.php
+++ bitrix/templates/.default/components/bitrix/news.list/catalog.list/template.php
@@ -3,42 +3,47 @@
 if(count($arResult["ITEMS"]) == 1){
     LocalRedirect($arResult["ITEMS"][0]["DETAIL_PAGE_URL"]."?ITEM_ONE=Y");
 }
-    $section = CIBlockSection::GetList(
-        array(),
-        array(
-            "IBLOCK_TYPE" => "lists",
-            "IBLOCK_CODE" => "nr_materials",
-            "IBLOCK_ID" => $arResult["ID"],
-            "CODE" => $_REQUEST["CODE"]
-        ),
-        false,
-        array("UF_*"),
-        false
-    );
+$section = CIBlockSection::GetList(
+    array(),
+    array(
+        "IBLOCK_TYPE" => "lists",
+        "IBLOCK_CODE" => "nr_materials",
+        "IBLOCK_ID" => $arResult["ID"],
+        "CODE" => $_REQUEST["CODE"]
+    ),
+    false,
+    array("UF_*"),
+    false
+);
+if($section->SelectedRowsCount()){
     $arSection = $section->GetNext();
-?>
-<div class="grid_wrapper">
-    <a title="<?=GetMessage("TITLE_BACK")?>" href="<?=SITE_DIR?>nerudnye-materialy.html" class="backlink"></a><?
-    foreach($arResult["ITEMS"] as $Item){
-        $ItemName = preg_replace("/(\d+)-(\d+)/", "<span class='nowrap'>\$1-\$2</span>", $Item["NAME"]);
-        $ItemName = preg_replace("/сорт (\d+)/", "<span class='nowrap'>сорт \$1</span>", $ItemName);?>
-        <div class="grid_element">
-            <a href="<?=$Item["DETAIL_PAGE_URL"]?>">
-                <span class="grid_element_img">
-                    <img src="<?=$Item["PREVIEW_PICTURE"]["SRC"]?>" alt="<?=$Item["NAME"]?>" />
-                    <span></span>
-                </span>
-                <span class="grid_element_title"><?=$ItemName?></span>
-            </a>
-        </div><?
-    }?>
-    
-</div>
-<div class="double_zigzag_wrapper">
-    <div><?=GetMessage("KRYIM")?></div>
-</div>
-<div class="content_text">
-    <?=$arSection["DESCRIPTION"]?>
-</div>
-<?$APPLICATION->SetPageProperty("pagetitle", $arSection["NAME"]);?>
-<?$APPLICATION->SetPageProperty("headertitle", $arSection["UF_META_TITLE"]);?>
+    ?>
+    <div class="grid_wrapper">
+        <a title="<?=GetMessage("TITLE_BACK")?>" href="<?=SITE_DIR?>nerudnye-materialy.html" class="backlink"></a><?
+        foreach($arResult["ITEMS"] as $Item){
+            $ItemName = preg_replace("/(\d+)-(\d+)/", "<span class='nowrap'>\$1-\$2</span>", $Item["NAME"]);
+            $ItemName = preg_replace("/сорт (\d+)/", "<span class='nowrap'>сорт \$1</span>", $ItemName);?>
+            <div class="grid_element">
+                <a href="<?=$Item["DETAIL_PAGE_URL"]?>">
+                    <span class="grid_element_img">
+                        <img src="<?=$Item["PREVIEW_PICTURE"]["SRC"]?>" alt="<?=$Item["NAME"]?>" />
+                        <span></span>
+                    </span>
+                    <span class="grid_element_title"><?=$ItemName?></span>
+                </a>
+            </div><?
+        }?>
+        
+    </div>
+    <div class="double_zigzag_wrapper">
+        <div><?=GetMessage("KRYIM")?></div>
+    </div>
+    <div class="content_text">
+        <?=$arSection["DESCRIPTION"]?>
+    </div>
+    <?$APPLICATION->SetPageProperty("pagetitle", $arSection["NAME"]);?>
+    <?$APPLICATION->SetPageProperty("headertitle", $arSection["UF_META_TITLE"]);?><?
+}else{    
+    CHTTP::SetStatus("404 Not Found");
+    require($_SERVER["DOCUMENT_ROOT"]."/404.php");
+}?>
