25009cf4554a67cbb07b6d2c0367628e81429909
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-09-19 17:15:35 +0400

    added page property "headertitle" to main page (index.php) - not by me!

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ define("SHOW_KRYIM_BLOCK", "Y");
 $user_title = "Поставка <span>песка,</span> <span>щебня,</span> <span>отсева</span>";
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("headertitle", "Альфа-Ростов - продажа щебня, песка, глины, керамзита в Ростове-на-Дону и области.");
 $APPLICATION->SetTitle("Поставка <span>песка,</span> <span>щебня,</span> <span>отсева</span>");
 ?>
 <?$APPLICATION->IncludeFile(
